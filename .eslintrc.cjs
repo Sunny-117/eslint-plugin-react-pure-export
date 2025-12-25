399ce93b56bc8369d82772505fdd278ed2c19934
Sunny-117 <zhiqiangfu6@gmail.com>
2025-12-25 09:43:50 +0800

    chore: add ESLint configuration and plugin dependency to playground
    - Add root ESLint configuration file (.eslintrc.cjs) with TypeScript and React support
    - Configure parser for @typescript-eslint with ES2020 and JSX features
    - Add eslint-plugin-react-pure-export as workspace dependency in playground
    - Enable ESLint linting for the project and playground environment

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+    node: true
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+};
